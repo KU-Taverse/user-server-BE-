d1b7cd4b4f5dd2cd8b0b8772c557fd91191b5a61
kamothi <rhkddlf7911@naver.com>
2024-03-30 14:53:56 +0900

    init: spring boot 초기 설정

---
+++ src/test/java/ku/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
