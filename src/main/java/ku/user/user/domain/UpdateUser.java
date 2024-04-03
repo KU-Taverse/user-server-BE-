f825a6dc37be632704c532e16d1c1f77d9643207
kamothi <rhkddlf7911@naver.com>
2024-04-03 17:28:34 +0900

    Feat: UpdateUser DTO 추가

--- src/main/java/ku/user/user/domain/UpdateUser.java
+++ src/main/java/ku/user/user/domain/UpdateUser.java
@@ -1,4 +1,16 @@
 package ku.user.user.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class UpdateUser {
+    private String email;
+    private String nickname;
+
+    @Builder
+    public UpdateUser(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
 }
