4d67c0a6075fa204a0e2883a8864305234e37e2d
kwang il <57484942+kamothi@users.noreply.github.com>
2024-04-04 23:28:28 +0900

    Refact: UserEntity 클래스 기본생성자 접근 레벨 수정

--- src/main/java/ku/user/user/infrastructure/entity/UserEntity.java
+++ src/main/java/ku/user/user/infrastructure/entity/UserEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class UserEntity {
     @Id
