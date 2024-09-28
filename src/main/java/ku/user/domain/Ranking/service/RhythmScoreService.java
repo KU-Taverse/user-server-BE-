0841c5bdbf2965e3e9d89e9c4dc7df6ab603a985
kamothi <rhkddlf7911@naver.com>
2024-09-28 13:10:54 +0900

    feat: service layer interface 설계

---
+++ src/main/java/ku/user/domain/Ranking/service/RhythmScoreService.java
@@ -0,0 +1,24 @@
+package ku.user.domain.Ranking.service;
+
+import ku.user.domain.Ranking.domain.RhythmScore;
+import ku.user.domain.Ranking.domain.Status;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface RhythmScoreService {
+    // 생성
+    RhythmScore saveScore();
+
+    // 조회(해당 캐릭터의 모든 기록)
+    List<RhythmScore> findScoresByNickName(String nickName);
+
+    // 조회(해당 캐릭터의 특정 날짜 기록)
+    List<RhythmScore> findScoresByNickNameAndDate(String nickName, LocalDate date);
+
+    // 캐릭터 삭제
+    void updateScoreStatus(String nickName);
+
+    // 해당 캐릭터의 최고 기록
+    RhythmScore findHighestScoreByNickName(String nickName);
+}
---
+++ src/main/java/ku/user/domain/Ranking/service/SteppingStonesScoreService.java
@@ -0,0 +1,23 @@
+package ku.user.domain.Ranking.service;
+
+import ku.user.domain.Ranking.domain.SteppingStonesScore;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface SteppingStonesScoreService {
+    // 생성
+    SteppingStonesScore saveScore();
+
+    // 조회(해당 캐릭터의 모든 기록)
+    List<SteppingStonesScore> findScoresByNickName(String nickName);
+
+    // 조회(해당 캐릭터의 특정 날짜 기록)
+    List<SteppingStonesScore> findScoresByNickNameAndDate(String nickName, LocalDate date);
+
+    // 캐릭터 삭제
+    void updateScoreStatus(String nickName);
+
+    // 해당 캐릭터의 최고 기록
+    SteppingStonesScore findHighestScoreByNickName(String nickName);
+}
