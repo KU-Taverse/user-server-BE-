550830f022836275d969a472ee8116959b57a97a
kamothi <rhkddlf7911@naver.com>
2024-09-30 14:56:34 +0900

    feat: 서비스 로직 인터페이스 설계

---
+++ src/main/java/ku/user/domain/Ranking/service/RankingService.java
@@ -0,0 +1,12 @@
+package ku.user.domain.Ranking.service;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public interface RankingService {
+    public void updateScore(String userId, String characterName, LocalDateTime createdAt, String gameKey, int newScore);
+
+    public Set<Object> getTopRankers(String gameKey, int n);
+
+    public Long getUserRank(String gameKey, String userId);
+}
