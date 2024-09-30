9f207eeaf12db4671841d5a6f82a1db2640e8508
david-parkk <ji0513ji@naver.com>
2024-10-01 01:35:00 +0900

    fix: Quest entity

--- src/main/java/ku/user/domain/quest/domain/Quest.java
+++ src/main/java/ku/user/domain/quest/domain/Quest.java
@@ -1,20 +1,19 @@
 package ku.user.domain.quest.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.relational.core.sql.In;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
+@Getter
+@Table(name = "quests")
 public class Quest {
 
     @Id
@@ -23,12 +22,25 @@ public class Quest {
 
     private Long userId;
 
-    private boolean quest1;
+    private Integer quest1;
     private LocalDateTime quest1CompletedAt;
 
-    private boolean quest2;
+    private Integer quest2;
     private LocalDateTime quest2CompletedAt;
 
-    private boolean quest3;
+    private Integer quest3;
     private LocalDateTime quest3CompletedAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime createdAt;
+
+    public static Quest from(Long userId, List<Integer> randomQuestNumber){
+        return Quest.builder()
+                .userId(userId)
+                .quest1(randomQuestNumber.get(0))
+                .quest2(randomQuestNumber.get(1))
+                .quest3(randomQuestNumber.get(2))
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 }
