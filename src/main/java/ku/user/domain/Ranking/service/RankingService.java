9e11ce04c25a2ebf44ec2a272cb6a5d7ad32d104
kamothi <rhkddlf7911@naver.com>
2024-09-30 14:58:55 +0900

    fix: 서비스 로직 인터페이스 파라미터 수정

--- src/main/java/ku/user/domain/Ranking/service/RankingService.java
+++ src/main/java/ku/user/domain/Ranking/service/RankingService.java
@@ -4,9 +4,9 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface RankingService {
-    public void updateScore(String userId, String characterName, LocalDateTime createdAt, String gameKey, int newScore);
+    public void updateScore(String characterName, LocalDateTime createdAt, String gameKey, int newScore);
 
     public Set<Object> getTopRankers(String gameKey, int n);
 
-    public Long getUserRank(String gameKey, String userId);
+    public Long getUserRank(String gameKey, String characterName);
 }
