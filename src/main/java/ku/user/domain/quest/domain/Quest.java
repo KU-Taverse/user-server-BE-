9ab0c147a31f41bd68a476ef50ab3e5fbe3de658
david-parkk <ji0513ji@naver.com>
2024-10-01 02:38:56 +0900

    fix: LocalDataTime -> LocalData

--- src/main/java/ku/user/domain/quest/domain/Quest.java
+++ src/main/java/ku/user/domain/quest/domain/Quest.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.relational.core.sql.In;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -31,16 +32,24 @@ public class Quest {
     private Integer quest3;
     private LocalDateTime quest3CompletedAt;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDateTime createdAt;
+    private LocalDate createdAt;
 
-    public static Quest from(Long userId, List<Integer> randomQuestNumber){
+    public static Quest from(Long userId, List<Integer> randomQuestNumber) {
         return Quest.builder()
                 .userId(userId)
                 .quest1(randomQuestNumber.get(0))
                 .quest2(randomQuestNumber.get(1))
                 .quest3(randomQuestNumber.get(2))
-                .createdAt(LocalDateTime.now())
+                .createdAt(LocalDate.now())
                 .build();
+    }
+
+    public void solve(Integer questIndex) {
+        if (quest1.equals(questIndex))
+            quest1CompletedAt = LocalDateTime.now();
+        if (quest2.equals(questIndex))
+            quest2CompletedAt = LocalDateTime.now();
+        if (quest3.equals(questIndex))
+            quest3CompletedAt = LocalDateTime.now();
     }
 }
