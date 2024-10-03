fb26f1e1a8ec6ab828a85b49e635534b8a80e06f
david-parkk <ji0513ji@naver.com>
2024-10-04 01:06:34 +0900

    fix: GetItemResponse

--- src/main/java/ku/user/domain/inventory/dto/response/GetItemResponse.java
+++ src/main/java/ku/user/domain/inventory/dto/response/GetItemResponse.java
@@ -6,12 +6,12 @@ import lombok.Builder;
 
 @Builder
 public record GetItemResponse(Long id,
-                              ItemType itemType,
+                              String itemName,
                               Long itemNumber) {
     public static GetItemResponse toDto(Item item) {
         return GetItemResponse.builder()
                 .id(item.getId())
-                .itemType(item.getItemType())
+                .itemName(item.getItemType().getItemName())
                 .itemNumber(item.getItemType().getItemNumber())
                 .build();
 
