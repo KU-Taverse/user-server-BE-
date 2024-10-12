d9e13dbc3c805a5ac925b367d135e99af2708fa5
david-parkk <ji0513ji@naver.com>
2024-10-12 14:10:16 +0900

    fix GET /charaters

--- src/main/java/ku/user/domain/character/dto/response/FindCharacterByEmailResponse.java
+++ src/main/java/ku/user/domain/character/dto/response/FindCharacterByEmailResponse.java
@@ -2,20 +2,27 @@ package ku.user.domain.character.dto.response;
 
 import ku.user.domain.character.domain.Character;
 import ku.user.domain.character.domain.CharacterType;
+import ku.user.domain.inventory.domain.Inventory;
 import lombok.Builder;
+
+import java.util.List;
 
 @Builder
 public record FindCharacterByEmailResponse (Long id, String nickname,
                                            CharacterType characterType, Long userId,
-                                           int currentMoney) {
+                                           int currentMoney,
+                                            List<Long> useItemList) {
 
-    public static FindCharacterByEmailResponse toDto(Character character){
+    public static FindCharacterByEmailResponse toDto(Inventory inventory, Character character){
         return FindCharacterByEmailResponse.builder()
                 .id(character.getId())
                 .nickname(character.getNickname())
                 .characterType(character.getCharacterType())
                 .userId(character.getUserId())
                 .currentMoney(character.getCurrentMoney())
+                .useItemList(List.of(inventory.getAuraIndex(),
+                        inventory.getTitleColorIndex(),
+                        inventory.getTitleBackgroundIndex()))
                 .build();
     }
 }
